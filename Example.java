e9a02c86e41c73d67c3cfa183dd2e491dbb2cc13
André Rodrigues <fury252@gmail.com>
2012-12-19 17:45:19 +0000

    Removed trash from rep

--- Example.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Example {
-	public static void main(String[] args) {
-		LinkedList<String[]> list = new LinkedList<String[]>();
-		String[][] s = (String[][]) list.toArray();
-	}
-}
